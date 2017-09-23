a716b2b90ea57817a9f4ab0529e9b0a7d62aaa04
Chaojin <jinchaoonly@gmail.com>
2017-09-23 16:37:07 -0400

    add debugscript

---
+++ Fly/Assets/_Script/DebugScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugScript : MonoBehaviour {
+
+	public GameObject Floor;
+	public GameObject Camera;
+
+
+	// Use this for initialization
+	void Start () {
+
+		Debug.Log ("startfloor:" + Floor.gameObject.transform.position);
+		Debug.Log ("startcamera:" + Camera.gameObject.transform.position);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		Debug.Log ("floor:" + Floor.gameObject.transform.position);
+		Debug.Log ("camera:" + Camera.gameObject.transform.position);
+	}
+}
