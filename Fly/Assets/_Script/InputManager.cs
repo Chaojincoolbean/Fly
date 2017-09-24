3c7529a55a74fd0f96f55687235db880448659db
Chaojin <jinchaoonly@gmail.com>
2017-09-24 18:03:13 -0400

    input fly, probably will not work

--- Fly/Assets/_Script/InputManager.cs
+++ Fly/Assets/_Script/InputManager.cs
@@ -11,7 +11,7 @@ public class InputManager : MonoBehaviour {
 	public Transform LRT; //LRT = LastRightContollerTransform
 	public Transform CLT; //CLT = CurrentLeftControllerTransform
 	public Transform CRT; //CRT = CurrentRightControllerTransform
-	//public Transform 
+	public float StandandY = 1.4f; //Where the player's arm is on Y position when they raise
 
 	// Use this for initialization
 	void Start () {
@@ -27,11 +27,16 @@ public class InputManager : MonoBehaviour {
 		CLT.position = LeftController.transform.position;
 		CRT.position = RightController.transform.position;
 
+		if ((CLT.position.y < StandandY) &(LLT.position.y > StandandY)
+			&(CRT.position.y < StandandY) & (LRT.position.y > StandandY)) {
 
+			Camera.gameObject.transform.position = new Vector3 (Camera.gameObject.transform.position.x, 
+				Camera.gameObject.transform.position.y + 1f, 
+				Camera.gameObject.transform.position.z);
+		}
 
-
-
-
+		LLT.position = CLT.position;
+		LRT.position = CRT.position;
 		
 	}
 }
