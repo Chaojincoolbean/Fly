785c76cb1bea4b06e7d7ed534a2a4206127d25a1
Chaojin <jinchaoonly@gmail.com>
2017-09-24 18:27:35 -0400

    test bug

--- Fly/Assets/_Script/InputManager.cs
+++ Fly/Assets/_Script/InputManager.cs
@@ -7,18 +7,17 @@ public class InputManager : MonoBehaviour {
 	public GameObject CameraRig;
 	public GameObject LeftController;
 	public GameObject RightController;
-	public Transform LLT; //LLT = LastLeftContollerTransform
-	public Transform LRT; //LRT = LastRightContollerTransform
-	public Transform CLT; //CLT = CurrentLeftControllerTransform
-	public Transform CRT; //CRT = CurrentRightControllerTransform
+	public Vector3 LLT; //LLT = LastLeftContollerPosition
+	public Vector3  LRT; //LRT = LastRightContollerPosition
+	public Vector3  CLT; //CLT = CurrentLeftControllerPosition
+	public Vector3  CRT; //CRT = CurrentRightControllerPosition
 	public float StandandY = 1.4f; //Where the player's arm is on Y position when they raise
 	public float initTimeDelay = 4f;
 
 	// Use this for initialization
 	void Start () {
 
-		LLT.position = Vector3.zero;
-		LRT.position = Vector3.zero;
+		LLT = LRT = CLT = CRT = Vector3.zero;
 		
 	}
 	
@@ -27,20 +26,19 @@ public class InputManager : MonoBehaviour {
 
 		if (Time.time > initTimeDelay) {
 			
+			CLT = LeftController.transform.position;
+			CRT = RightController.transform.position;
 
-			CLT.position = LeftController.transform.position;
-			CRT.position = RightController.transform.position;
-
-			if ((CLT.position.y < StandandY) & (LLT.position.y > StandandY)
-			   & (CRT.position.y < StandandY) & (LRT.position.y > StandandY)) {
+			if ((CLT.y < StandandY) & (LLT.y > StandandY)
+			   & (CRT.y < StandandY) & (LRT.y > StandandY)) {
 
 				CameraRig.gameObject.transform.position = new Vector3 (CameraRig.gameObject.transform.position.x, 
 					CameraRig.gameObject.transform.position.y + 1f, 
 					CameraRig.gameObject.transform.position.z);
 			}
 
-			LLT.position = CLT.position;
-			LRT.position = CRT.position;
+			LLT = CLT;
+			LRT = CRT;
 
 		}
 		
