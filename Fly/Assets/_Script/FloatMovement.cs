c7d97a767c50cc148f6777a97592c6f6c691edfa
Chaojin <jinchaoonly@gmail.com>
2017-10-21 21:28:59 -0400

    Try how that feels

--- Fly/Assets/_Script/FloatMovement.cs
+++ Fly/Assets/_Script/FloatMovement.cs
@@ -7,17 +7,17 @@ public class FloatMovement : MonoBehaviour {
 
 	public GameObject Player;
 	public float StartTime;
-	public float FloatSpeed1;
-    public float FloatSpeed2;
-    public float FloatSpeed3;
-    public float FloatSpeed4;
-    public float SpeedupPoint1;
-    public float SpeedupPoint2;
-    public float SpeedupPoint3;
-    public float FloatEndPositionX;
-    public float FloatEndPositionY;
+	public float FloatSpeed;
+//	public float FloatSpeed1;
+//    public float FloatSpeed2;
+//    public float FloatSpeed3;
+//    public float FloatSpeed4;
+//    public float SpeedupPoint1;
+//    public float SpeedupPoint2;
+//    public float SpeedupPoint3;
+//    public float FloatEndPositionX;
+//    public float FloatEndPositionY;
     public float MoveForwardStopTime;
-    public bool FloatUpEnd = false;
     public bool FloatForwardEnd = false;
     private float FloatUpStopTime = 0;
 
@@ -30,55 +30,52 @@ public class FloatMovement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        if (Time.time > StartTime && FloatUpEnd == false)
+        if (Time.time > StartTime)
         {
             FloatUp();
-        }
-
-        if(FloatUpEnd == true && Time.time - FloatUpStopTime > MoveForwardStopTime && FloatForwardEnd == false)
-        {
-            //FloatForward();
-        }       
+        }      
 
 	}
 
     void FloatUp()
     {
-        Player.transform.position = new Vector3(Player.transform.position.x, Player.transform.position.y + FloatSpeed1, Player.transform.position.z);
 
-            if (Player.transform.position.y > SpeedupPoint1)
-            {
-                Player.transform.position = new Vector3(Player.transform.position.x, Player.transform.position.y + FloatSpeed2, Player.transform.position.z);
-            }
+		FloatSpeed = Time.time;
+        Player.transform.position = new Vector3(Player.transform.position.x, Player.transform.position.y + FloatSpeed, Player.transform.position.z);
 
-            if (Player.transform.position.y > SpeedupPoint2)
-            {
-                Player.transform.position = new Vector3(Player.transform.position.x, Player.transform.position.y + FloatSpeed3, Player.transform.position.z);
-            }
-
-            if (Player.transform.position.y > SpeedupPoint3)
-            {
-                Player.transform.position = new Vector3(Player.transform.position.x, Player.transform.position.y + FloatSpeed4, Player.transform.position.z);
-            }
-
-            if (Player.transform.position.y > FloatEndPositionY)
-            {
-                SceneManager.LoadScene(2);
-                //FloatUpEnd = true;
-                //FloatUpStopTime = Time.time;
-            }
+//            if (Player.transform.position.y > SpeedupPoint1)
+//            {
+//                Player.transform.position = new Vector3(Player.transform.position.x, Player.transform.position.y + FloatSpeed2, Player.transform.position.z);
+//            }
+//
+//            if (Player.transform.position.y > SpeedupPoint2)
+//            {
+//                Player.transform.position = new Vector3(Player.transform.position.x, Player.transform.position.y + FloatSpeed3, Player.transform.position.z);
+//            }
+//
+//            if (Player.transform.position.y > SpeedupPoint3)
+//            {
+//                Player.transform.position = new Vector3(Player.transform.position.x, Player.transform.position.y + FloatSpeed4, Player.transform.position.z);
+//            }
+//
+//            if (Player.transform.position.y > FloatEndPositionY)
+//            {
+//                SceneManager.LoadScene(2);
+//                //FloatUpEnd = true;
+//                //FloatUpStopTime = Time.time;
+//            }
     }
 
-    void FloatForward()
-    {
-
-        Player.transform.position = new Vector3(Player.transform.position.x + FloatSpeed1, Player.transform.position.y, Player.transform.position.z);
-
-        if (Player.transform.position.x > FloatEndPositionX)
-        {
-           FloatForwardEnd = true;
-           //FloatUpStopTime = Time.time;
-        }
-
-    }
+//    void FloatForward()
+//    {
+//
+//        Player.transform.position = new Vector3(Player.transform.position.x + FloatSpeed1, Player.transform.position.y, Player.transform.position.z);
+//
+//        if (Player.transform.position.x > FloatEndPositionX)
+//        {
+//           FloatForwardEnd = true;
+//           //FloatUpStopTime = Time.time;
+//        }
+//
+//    }
 }
