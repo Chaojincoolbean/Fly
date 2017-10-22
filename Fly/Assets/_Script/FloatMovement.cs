2c8ee6fe18c185577fe9fe561f5a0dc84dece9b2
Democracy <ll3305@nyu.edu>
2017-10-21 22:58:02 -0400

    adjust movement and music

--- Fly/Assets/_Script/FloatMovement.cs
+++ Fly/Assets/_Script/FloatMovement.cs
@@ -8,23 +8,29 @@ public class FloatMovement : MonoBehaviour {
 	public GameObject Player;
 	public float StartTime;
 	public float FloatSpeed;
+    public float Acceleration;
+    public float MaxSpeed;
 //	public float FloatSpeed1;
 //    public float FloatSpeed2;
 //    public float FloatSpeed3;
 //    public float FloatSpeed4;
-//    public float SpeedupPoint1;
-//    public float SpeedupPoint2;
+    public float AccelerationPoint;
+    public float AccelerationPoint2;
 //    public float SpeedupPoint3;
 //    public float FloatEndPositionX;
-//    public float FloatEndPositionY;
+    public float FloatEndPositionY;
     public float MoveForwardStopTime;
     public bool FloatForwardEnd = false;
+    public bool MusicPlay = true;
     private float FloatUpStopTime = 0;
+    public AudioSource AS;
+
 
     // Use this for initialization
     void Start () {
 
-		
+        AS = this.gameObject.GetComponent<AudioSource>();
+
 	}
 	
 	// Update is called once per frame
@@ -33,15 +39,40 @@ public class FloatMovement : MonoBehaviour {
         if (Time.time > StartTime)
         {
             FloatUp();
-        }      
+        }
+        /*
+        if (Player.transform.position.y > AccelerationPoint && MusicPlay == true)
+        {
+            AS.Play();
+            MusicPlay = false;
+        }
+        */
+        
 
-	}
+    }
 
     void FloatUp()
     {
 
-		FloatSpeed = Time.time;
+        if(Player.transform.position.y > AccelerationPoint)
+        {
+            FloatSpeed += Acceleration;
+            AS.volume -=  Acceleration;
+        }
+
+        if (Player.transform.position.y > AccelerationPoint2)
+        {
+            FloatSpeed += Acceleration*30;
+            //AS.volume -= Acceleration;
+        }
+
+        if (FloatSpeed > MaxSpeed)
+        {
+            FloatSpeed = MaxSpeed;
+        }
+
         Player.transform.position = new Vector3(Player.transform.position.x, Player.transform.position.y + FloatSpeed, Player.transform.position.z);
+
 
 //            if (Player.transform.position.y > SpeedupPoint1)
 //            {
@@ -58,12 +89,11 @@ public class FloatMovement : MonoBehaviour {
 //                Player.transform.position = new Vector3(Player.transform.position.x, Player.transform.position.y + FloatSpeed4, Player.transform.position.z);
 //            }
 //
-//            if (Player.transform.position.y > FloatEndPositionY)
-//            {
-//                SceneManager.LoadScene(2);
-//                //FloatUpEnd = true;
-//                //FloatUpStopTime = Time.time;
-//            }
+            if (Player.transform.position.y > FloatEndPositionY)
+            {
+            SceneManager.LoadScene(2);
+
+            }
     }
 
 //    void FloatForward()
