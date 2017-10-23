eba518017247408456930cd1bb9b87ab43f03522
Chaojin <jinchaoonly@gmail.com>
2017-10-22 20:50:25 -0400

    Let's see how sail feels

---
+++ Fly/Assets/_Script/SailManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SailManager : MonoBehaviour {
+
+	public GameObject Boat;
+	public GameObject Player;
+	public float SailStartTime;
+	public float SailSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Time.time > SailStartTime) {
+			Boat.SetActive (true);
+			
+		}
+		
+	}
+
+	public void Sail(){
+
+		Player.transform.position = new Vector3(Player.transform.position.x, Player.transform.position.y, Player.transform.position.z - SailSpeed);
+		Boat.transform.position = new Vector3(Boat.transform.position.x, Boat.transform.position.y, Boat.transform.position.z - SailSpeed);
+	}
+}
