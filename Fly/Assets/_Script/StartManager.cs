d4ae560c5d8b023e9560a0a67101c32d9c073cff
Chaojin <jinchaoonly@gmail.com>
2017-11-04 19:43:24 -0400

    add new heartbeat with nice cut

--- Fly/Assets/_Script/StartManager.cs
+++ Fly/Assets/_Script/StartManager.cs
@@ -5,26 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class StartManager : MonoBehaviour {
 
-	AudioSource AS;
-	public AudioClip DyingHeartAudio;
-	bool DyingHeartPlay = true;
     bool LoadScene = false;
 	public int LoadTime;
 
 	// Use this for initialization
 	void Start () {
 
-		AS = this.gameObject.GetComponent<AudioSource> ();
     }
 	
 	// Update is called once per frame
 	void Update () {
 
-		if (Time.time > 16 & DyingHeartPlay == true) {
-			AS.clip = DyingHeartAudio;
-			AS.Play ();
-			DyingHeartPlay = false;
-		}
 
 		if (Time.time > LoadTime) {
 
