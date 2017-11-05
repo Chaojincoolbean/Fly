1987d4adfb2f8206362db501bb829df1ea32ee83
Chaojin <jinchaoonly@gmail.com>
2017-11-04 21:29:33 -0400

    camera flash, let's see how it feels

---
+++ Fly/Assets/_Script/CameraFlash.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraFlash : MonoBehaviour {
+
+	public float flashTimeLength = 0.2f;
+	public bool doCameraFlash = true;
+
+	private Image flashImage;
+	private float startTime;
+	private bool flashing = false;
+
+	// Use this for initialization
+	void Start () {
+
+		flashImage = GetComponent<Image> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(doCameraFlash && !flashing)
+		{
+			Flash();
+		}
+//		else
+//		{
+//			doCameraFlash = false;
+//		}
+	}
+
+	public void Flash()
+	{
+		// initial color
+		Color col = flashImage.color;
+
+		// start time to fade over time
+		startTime = Time.time;
+
+		// so we can flash again
+		doCameraFlash = false;
+
+		// start it as alpha = 1.0 (opaque)
+		col.a = 1.0f;
+
+		// flash image start color
+		flashImage.color = col;
+
+		// flag we are flashing so user can't do 2 of them
+		flashing = true;
+
+		StartCoroutine(FlashCoroutine());
+
+	}
+
+	IEnumerator FlashCoroutine()
+	{
+		bool done = false;
+
+		while(!done)
+		{
+			float perc;
+			Color col = flashImage.color;
+
+			perc = Time.time - startTime;
+			perc = perc / flashTimeLength;
+
+			if(perc > 1.0f)
+			{
+				perc = 1.0f;
+				done = true;
+				doCameraFlash = true;
+			}
+
+			col.a = Mathf.Lerp(1.0f, 0.0f, perc);
+			flashImage.color = col;
+			flashing = true;
+
+			yield return null;
+		}
+
+		flashing = false;
+
+		yield break;
+	}
+}
