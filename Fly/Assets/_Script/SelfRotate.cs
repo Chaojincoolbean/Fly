5814f04815d836ed70ace1bcfe0307001cfa8ee6
Chaojin <jinchaoonly@gmail.com>
2017-11-05 20:30:11 -0500

    Now we have mask!

---
+++ Fly/Assets/_Script/SelfRotate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfRotate : MonoBehaviour {
+
+	public float n;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.Rotate (Vector3.up * n * Time.deltaTime);
+		
+	}
+}
