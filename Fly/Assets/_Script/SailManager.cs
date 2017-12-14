fdf2faf01fd7c9e998656e67f0a48555ef126cb1
Chaojin <jinchaoonly@gmail.com>
2017-12-13 21:56:39 -0500

    add sail scene

--- Fly/Assets/_Script/SailManager.cs
+++ Fly/Assets/_Script/SailManager.cs
@@ -8,10 +8,11 @@ public class SailManager : MonoBehaviour {
 	public GameObject Player;
 	public float SailStartTime;
 	public float SailSpeed;
-    public GameObject Memories;
+	public float DrowingSpeed;
     public GameObject BackgroundMusic;
-    public GameObject End;
     public float Loadtime;
+	public float Drown;
+
 
 	// Use this for initialization
 	void Start () {
@@ -26,11 +27,6 @@ public class SailManager : MonoBehaviour {
 
         Loadtime = Loadtime + Time.deltaTime;
 
-        if (Loadtime > SailStartTime-10f)
-        {
-            BackgroundMusic.SetActive(true);
-        }
-
         if (Loadtime > SailStartTime) {
 
 			Boat.SetActive (true);
@@ -38,22 +34,17 @@ public class SailManager : MonoBehaviour {
   
         }
 
-        if (Loadtime > SailStartTime + 60f)
+		if(Player.transform.position.y < Drown)
         {
-            //BackgroundMusic.SetActive(true);
-            Memories.SetActive(true);
-        }
-
-        if(Player.transform.position.z < -135f)
-        {
-            End.SetActive(true);
+            //End.SetActive(true);
         }
 
     }
 
 	public void Sail(){
 
-		Player.transform.position = new Vector3(Player.transform.position.x, Player.transform.position.y, Player.transform.position.z - SailSpeed);
-		Boat.transform.position = new Vector3(Boat.transform.position.x, Boat.transform.position.y, Boat.transform.position.z - SailSpeed);
+		Player.transform.position = new Vector3(Player.transform.position.x, Player.transform.position.y - DrowingSpeed, Player.transform.position.z - SailSpeed);
+		Boat.transform.position = new Vector3(Boat.transform.position.x, Boat.transform.position.y - DrowingSpeed, Boat.transform.position.z - SailSpeed);
+
 	}
 }
